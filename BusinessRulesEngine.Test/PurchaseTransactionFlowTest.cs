1b47f901be8d0152d080f1505e948c8b28fa7a37
davidSchor <david@schor.dk>
2021-02-06 14:52:00 +0100

    BusinessRulesEngine - Added Test project

---
+++ BusinessRulesEngine.Test/PurchaseTransactionFlowTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace BusinessRulesEngine.Test
+{
+    public class PurchaseTransactionFlowTest
+    {
+        [Fact]
+        public void GIVEN_PhysicalPurchaseOrder_WHEN_Purchase_THEN_PackingSlipSentToShipping()
+        {
+        }
+    }
+}
