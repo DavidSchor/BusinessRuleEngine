5a8c038f3d58456805abcf392d7a0c597fdcff3f
davidSchor <david@schor.dk>
2021-02-06 16:07:07 +0100

    BusinessRulesEngine -  added membership ordertype

---
+++ BusinessRulesEngine/Models/OrderTypes/Membership.cs
@@ -0,0 +1,10 @@
+﻿namespace BusinessRulesEngine.Models.OrderTypes
+{
+    public class Membership : IPurchasable
+    {
+        public PurchaseResult Purchase(string orderId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
